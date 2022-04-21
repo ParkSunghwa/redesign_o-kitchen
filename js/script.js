76bafc648b33993ec2c5813fadd7739009bc1fa8
ParkSunghwa <mtreeferryp@gmail.com>
2022-04-22 01:00:37 +0900

    220422selfwork

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+    $(".wrap_fullpage").fullpage({
+
+        navigation:true,
+    });
+
+    // $(".wrap_fullpage > .wrap_mainvisual .box_mainvisual > .box_slide").slick({
+
+
+    // });
+
 });
